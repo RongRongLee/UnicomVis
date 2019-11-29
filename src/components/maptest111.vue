df407f2c99adaf0fc2f4ed0c8cda5f87fe50f726
Your NameRongRongLee <1406499403@qq.com>
2019-11-29 16:28:12 +0800

    change map

--- src/components/maptest111.vue
+++ src/components/maptest111.vue
@@ -20,49 +20,45 @@ export default {
       mapboxgl.accessToken =
         "pk.eyJ1IjoiZXRhbCIsImEiOiJjazNmd3o2Z3YwOHIyM210Njlrc3V5MWNvIn0.Nc7r-NwLUw5ctj0sXdbpxg";
       var coordinates = document.getElementById("coordinates");
-      console.log(coordinates);
       const map = new mapboxgl.Map({
         container: this.$refs.basicMapbox,
         style: "mapbox://styles/mapbox/streets-v11",
-        center: [104.75268915646745, 31.45435611535065], // 设置地图中心
+        center: [-122.447303, 37.753574], // 设置地图中心
         zoom: 13 // 设置地图比例
       });
-
-      // 使用定位模块
-      // map.addControl(new mapboxgl.GeolocateControl({
-      //     positionOptions: {
-      //         enableHighAccuracy: true
-      //     },
-      //     trackUserLocation: true,
-      //     showUserLocation: true,
-      //     zoom: 10,
-      // }))
-
-      // 建立一个标记点
-      var marker = new mapboxgl.Marker({
-        draggable: true
-      })
-        .setLngLat([104.68271, 31.54134])
-        .addTo(map);
-      function onDragEnd() {
-        var lngLat = marker.getLngLat();
-        coordinates.style.display = "block";
-        coordinates.innerHTML =
-          "Longitude: " + lngLat.lng + "<br />Latitude: " + lngLat.lat;
-      }
-      marker.on("dragend", onDragEnd);
-
-      // 点击获取经纬度模块&点击标记点
-      map.on("click", function(e) {
-        document.getElementById("info").innerHTML =
-          JSON.stringify(e.point) + "<br />" + JSON.stringify(e.lngLat);
-        marker.setLngLat([e.lngLat.lng, e.lngLat.lat]).addTo(map);
+      map.on("load", function() {
+        map.addLayer({
+          id: "basePoint",
+          type: "circle",
+          source: { 
+            type: "vector",
+            url: "mapbox://examples.8fgz4egr"
+          },
+          "source-layer": "sf2010",
+          paint: {
+            // make circles larger as the user zooms from z12 to z22
+            "circle-radius": {
+              base: 1.75,
+              stops: [[12, 2], [22, 180]]
+            },
+            // color circles by ethnicity, using a match expression
+            // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
+            "circle-color": [
+              "match",
+              ["get", "ethnicity"],
+              "White",
+              "#fbb03b",
+              "Black",
+              "#223b53",
+              "Hispanic",
+              "#e55e5e",
+              "Asian",
+              "#3bb2d0",
+              /* other */ "#ccc"
+            ]
+          }
+        });
       });
-      map.addControl(
-        new mapboxgl.MapboxGeocoder({
-          accessToken: mapboxgl.accessToken
-        })
-      );
     }
   },
   computed: {}
